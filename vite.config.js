28403ff0d45d5f07fe2774e0e2a0abb32c1ddd54
Deepak <vermadeepakd1@gmail.com>
2025-03-11 21:11:00 +0530

    Final Commit

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),  // Use Tailwind as a Vite plugin
+  ],
+  base: '/', // Ensures correct paths for assets in production
+  build: {
+    outDir: 'dist', // Ensure files are built into "dist/"
+  }
 });
